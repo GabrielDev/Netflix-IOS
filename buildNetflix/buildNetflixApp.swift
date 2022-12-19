5bdabb4a6b08a02f1b48d2e9a019def90b46b8f3
Gabriel dos Santos <Gabriel.SantosLucena-ext@ab-inbev.com>
2022-12-19 16:28:33 -0300

    Initial Commit

---
+++ buildNetflix/buildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  buildNetflixApp.swift
+//  buildNetflix
+//
+//  Created by Gabriel dos Santos on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct buildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
